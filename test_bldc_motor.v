8e43e899db1ff029e01b8a0f1c024c9cc05955dd
BatuCem <63973576+BatuCem@users.noreply.github.com>
2023-11-08 09:41:41 +0300

    Add files via upload

--- test_bldc_motor.v
+++ test_bldc_motor.v
@@ -14,27 +14,18 @@
 	reg [DATA_WIDTH-1:0] pwm_counter;
 
 	reg [DATA_WIDTH-1:0] pwm_duty_cycle_ones;
-	reg [DATA_WIDTH-1:0] duty_cycle;
+	reg [DATA_WIDTH-1:0] motor_period;
 	reg[1:0] count_direction;
 	
-	always @(posedge clk or posedge reset or posedge motor_positive or posedge motor_negative) 
-	begin
-		if(reset) begin
-			count_direction<=2'b00;
-		end else begin
-			duty_cycle<=(100-pwm_duty_cycle_ones*100/pwm_counter)*1+10;//suspicious division operation
-			pwm_counter<={DATA_WIDTH-1{1'b0}};
-			pwm_duty_cycle_ones<={DATA_WIDTH-1{1'b0}};
-			if(motor_positive) begin
+	always @(posedge motor_positive or posedge motor_negative) begin
+		if(motor_positive==1'b1) begin
 				count_direction<=2'b10;//forward motion
-			end else if(motor_negative) begin
+			end else if(motor_negative==1'b1) begin
 				count_direction<=2'b01;//backward motion
 			end else begin
 				count_direction<=2'b00;//default case when turning without encoders triggering or stopping motor
 			end
-			
-		end
-		
+			motor_period<=(100-pwm_duty_cycle_ones*100/pwm_counter)*2+20 -2;
 	end
 	
 	always @(posedge clk or posedge reset) 
@@ -72,27 +63,50 @@
 		if(reset) begin
 			fsm_ctr<={DATA_WIDTH-1{1'b0}};
 		end else begin
-			if(fsm_ctr==duty_cycle) begin
+			if(fsm_ctr==motor_period) begin
 				fsm_ctr<={DATA_WIDTH-1{1'b0}};//reset fsm each period
 			end else begin
+			if (count_direction==2'b10) begin
 				case(fsm_ctr)
-        duty_cycle-3:begin	//01 encoder state sent first
+        motor_period-3:begin	//01 encoder state sent first
 				encoder_a<=1'b0;
-				encoder_a<=1'b1;
+				encoder_b<=1'b1;
         end
-        duty_cycle-2:begin	//11 encoder state sent second
+        motor_period-2:begin	//11 encoder state sent second
 				encoder_a<=1'b1;
 				encoder_b<=1'b1;
         end
-        duty_cycle-1:begin	//10 encoder state sent third
+        motor_period-1:begin	//10 encoder state sent third
 				encoder_a<=1'b1;
 				encoder_b<=1'b0;
         end
         default: begin
 				encoder_a<=1'b0;//00 state in between turns or not turning
-				encoder_a<=1'b0;
+				encoder_b<=1'b0;
         end
       endcase
+		end else begin
+		case(fsm_ctr)
+        motor_period-3:begin	//01 encoder state sent first
+				encoder_a<=1'b1;
+				encoder_b<=1'b0;
+        end
+        motor_period-1:begin	//11 encoder state sent second
+				encoder_a<=1'b1;
+				encoder_b<=1'b1;
+        end
+        motor_period:begin	//10 encoder state sent third
+				encoder_a<=1'b0;
+				encoder_b<=1'b1;
+        end
+        default: begin
+				encoder_a<=1'b0;//00 state in between turns or not turning
+				encoder_b<=1'b0;
+        end
+      endcase
+		
+		end
+		
 			
 			
 				fsm_ctr<=fsm_ctr+1;//increment state counter
