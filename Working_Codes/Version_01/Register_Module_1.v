d46c04f70ab98f7dd3c98bccaf983d7bc2fd71e4
Ethem Buğra Arslan <101515029+LyraN66@users.noreply.github.com>
2023-12-10 12:52:49 +0300

    Create Register_Module_1.v

---
+++ Working_Codes/Version_01/Register_Module_1.v
@@ -0,0 +1,55 @@
+module Register_Module_1#(parameter depth = 20)(
+    input clk,
+    input rst,
+    input write,
+    input read_1,
+    input [7:0] index_1,
+    input [7:0] data_in,
+    output [7:0] data_out_1,
+    //
+    output [15:0] pwm_period,           // 0x40-0x41
+    output [15:0] period_reference,     // 0x42-0x43
+    output [7:0] Kp_ext,               // 0x44
+    output [7:0] Ki_ext,               // 0x45
+    output [6:0] Kd_ext,               // 0x46 [6:0]
+    output override_internal_pid        // 0x46 [7]
+    );
+    
+    reg [7:0] internal_register [0:depth-1]; // start from 0x40
+    assign data_out_1 = (read_1) ? (internal_register[index_1[4:0]]) : (8'b0);
+    
+    assign pwm_period            = {internal_register[0],internal_register[1]};
+    assign period_reference      = {internal_register[2],internal_register[3]};
+    assign Kp_ext                = internal_register[4];
+    assign Ki_ext                = internal_register[5];
+    assign Kd_ext                = internal_register[6][6:0];
+    assign override_internal_pid = internal_register[6][7];
+    
+    always @(posedge clk)begin
+        if(rst)begin
+            internal_register [0] <= 8'b0;
+            internal_register [1] <= 8'b0;
+            internal_register [2] <= 8'b0;
+            internal_register [3] <= 8'b0;
+            internal_register [4] <= 8'b0;
+            internal_register [5] <= 8'b0;
+            internal_register [6] <= 8'b0;
+            internal_register [7] <= 8'b0;
+            internal_register [8] <= 8'b0;
+            internal_register [9] <= 8'b0;
+            internal_register [10] <= 8'b0;
+            internal_register [11] <= 8'b0;
+            internal_register [12] <= 8'b0;
+            internal_register [13] <= 8'b0;
+            internal_register [14] <= 8'b0;
+            internal_register [15] <= 8'b0;
+            internal_register [16] <= 8'b0;
+            internal_register [17] <= 8'b0;
+            internal_register [18] <= 8'b0;
+            internal_register [19] <= 8'b0;
+        end
+        else begin
+            internal_register[index_1[4:0]] <= (write) ? (data_in) : (internal_register[index_1[4:0]]);
+        end
+    end
+endmodule
